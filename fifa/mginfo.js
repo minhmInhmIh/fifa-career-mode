77bcb0f65e3889d3e9973f05c94094b5a77e0d3f
Minh <petertruong.bruh@gmail.com>
2026-03-22 21:10:50 +0700

    added background music

---
+++ fifa/mginfo.js
@@ -0,0 +1,9 @@
+document.getElementById("advance-btn").onclick = function(){
+    firstname = document.getElementById("fninput").value || "A.A.";
+    lastname = document.getElementById("lninput").value || "Manager";
+    nationality = document.getElementById("ninput").value || "England";
+    console.log(firstname)
+    console.log(lastname)
+    console.log(nationality)
+    }
+        
